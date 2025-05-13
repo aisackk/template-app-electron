a19bd5f6f5d60481fb2ab4d1ca419f50e26209ab
Isaque Niceas <testeisaque15@gmail.com>
2025-05-12 22:08:56 -0300

    Criando um template

---
+++ main.js
@@ -0,0 +1,21 @@
+//Importa módulos app e BrowserWindow do Electron
+const{app, BrowserWindow} = require('electron');
+
+//funcao para criar a janela principal
+function createWindow() {
+   
+    //Criar uma nova instancia do BrowserWindow
+    const window = new BrowserWindow({
+        width: 800, //Largura da janela
+        height: 600, //Altura da janela
+
+    })
+
+    //Carregar arquivo HTML na janela
+    window.loadFile('index.html');
+}
+
+//Evento que é acionado quando o electron está pronto.
+app.whenReady().then(() => {
+    createWindow(); //Chama função de criar a janela
+})
